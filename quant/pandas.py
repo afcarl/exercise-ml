e401ecf483c4c7099b00fffb08b65f6bb4617f3b
shyrwinsia <shyrwinsia@gmail.com>
2018-04-29 17:10:27 +0800

    Added missing data

--- quant/pandas.py
+++ quant/pandas.py
@@ -3,7 +3,7 @@ import pandas as pd
 from datetime import datetime
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("./quant/historical/ali.csv", \
+df = pd.read_csv("./quant/historical/urc.csv", \
 delimiter='\t', \
 header=None, \
 names=['date', 'close', 'open', 'high', 'low', 'volume', 'change'] )
@@ -14,8 +14,11 @@ df = df.applymap(str.strip).applymap(lambda x: x.replace(',',''))
 
 # process change
 df.change = df.change.apply(lambda x : float(x.strip('%'))/100)
-
+df.change
 # process volume
+# df.volume = df.volume.replace(r'[KM]+$', '', regex=True)
+# print(df.volume.to_string())
+
 df.volume = (df.volume.replace(r'[KM]+$', '', regex=True).astype(float) * \
     df.volume.str.extract(r'[\d\.]+([KM]+)', expand=False).fillna(1).replace(['K','M'], [10**3, 10**6]).astype(int))
 
@@ -27,9 +30,8 @@ df.close = df.close.astype(float)
 df.open = df.open.astype(float)
 df.high = df.high.astype(float)
 df.low = df.low.astype(float)
-#TODO fix BPI BDO
 # histogram the changes
-df.hist('change', bins=100)
+
 std = df.change.std() * 100
 std
 # plt.show()
